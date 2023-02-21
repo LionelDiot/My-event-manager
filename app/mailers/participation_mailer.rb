3602a0b7701dbf325571e15bc4b1032bfd29a8ac
Lionel Diot <diot.lionel2@gmail.com>
2023-02-21 03:17:31 +0100

    essai mailer

--- app/mailers/participation_mailer.rb
+++ app/mailers/participation_mailer.rb
@@ -1,12 +1,12 @@
 class ParticipationMailer < ApplicationMailer
-  default from: 'moneventmanager@gmail.com'
+  default from: 'diot.lionel2@gmail.com'
 
   def confirmation_email(participation)
     #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
     @user = participation.user 
     @event = participation.event
     #on définit une variable @url qu'on utilisera dans la view d’e-mail
-    @url = 'monsite.fr'
+    @url = 'https://www.thehackingproject.org/fr'
 
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
     mail(to: @user.email, subject: "Confirmation de participation à #{@event.title}") 
